1a875a8f771620fef7619c11658d11611e8c22f4
Jonata Serpa <jonataserpa@gmail.com>
2019-08-18 23:31:58 -0300

    alteração vendas

--- vendas.php
+++ vendas.php
@@ -38,12 +38,12 @@
 
       <div class="my-3 p-2 bg-white rounded box-shadow">
         <div class="row">
-          <a><img src="plus.PNG" title="Inserir" data-toggle="modal" data-target="#exampleModal" class="img" alt="Inserir"></a>
+          <a><img src="plus.PNG" title="Inserir" data-toggle="modal" data-target="#modalVenda" class="img" alt="Inserir"></a>
           <h6 class="border-bottom border-gray pb-2 mb-0"> &nbsp;&nbsp; Vendas</h6>
         </div>
       </div>
 
-      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal fade" id="modalVenda" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog " style="max-width: 950px!important;" role="document">
           <div class="modal-content">
             <div class="modal-header">
@@ -56,9 +56,10 @@
               <div class="col-md-12 order-md-1">
                 <form class="needs-validation" novalidate>
                   <div class="row">
+                    <input type="hidden" name="id" id="id" value="" />
                     <div class="col-md-3 mb-3">
-                      <label for="state">Status:</label>
-                      <select class="custom-select d-block w-100" id="state" required>
+                      <label for="status">Status:</label>
+                      <select class="custom-select d-block w-100" id="status" name="status" required>
                         <option value=""> </option>
                         <option>Aguardando</option>
                         <option>Cancelada</option>
@@ -69,15 +70,30 @@
                       </div>
                     </div>
                     <div class="col-md-6 mb-3">
-                      <label for="firstName">Aluno:</label>
-                      <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+                      <label for="idAluno">Aluno:</label>
+                      <input class="form-control" class="awesomplete" list="mylist" id="idAluno" name="idAluno">
                       <div class="invalid-feedback">
                         Valid first name is required.
                       </div>
+                      <datalist id="mylist">
+                        <option></option>
+                        <?php
+                        $strSQL = " SELECT id, CONCAT(snome, ' / ', sTurma) AS nome ";
+                        $strSQL .= " FROM tbAluno ";
+                        $strSQL .= " WHERE IFNULL(fDeletado, 0) = 0 ";
+                        if ($retornoQuery = $MySQLi->query($strSQL)) {
+                          while ($rsListagem = $retornoQuery->fetch_object()) {
+                            printf('<option>' . $rsListagem->id . " - " . $rsListagem->nome . '</option>');
+                          }
+                        } else {
+                          echo trigger_error($MySQLi->error, E_USER_ERROR);
+                        }
+                        ?>
+                      </datalist>
                     </div>
                     <div class="col-md-3 mb-3">
-                      <label for="state">Condição Pagamento:</label>
-                      <select class="custom-select d-block w-100" id="state" required>
+                      <label for="condicao">Condição Pagamento:</label>
+                      <select class="custom-select d-block w-100" id="condicao" name="condicao" required>
                         <option value=""> </option>
                         <option>A VISTA</option>
                         <option>A PRAZO</option>
@@ -89,8 +105,8 @@
                   </div>
                 </form>
                 <div class="form-group">
-                  <label for="exampleFormControlTextarea1">Observação:</label>
-                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                  <label for="observacao">Observação:</label>
+                  <textarea class="form-control" id="observacao" name="observacao" rows="3"></textarea>
                 </div>
 
                 <div class="row">
@@ -108,6 +124,7 @@
                     $strSQL = "";
                     $strSQL .= " SELECT ";
                     $strSQL .= "	tbproduto.id, ";
+                    $strSQL .= "	CONCAT(tbproduto.id, ' * ', tbproduto.sDescricao, ' *' ,tbproduto.fpreco) AS prod, ";
                     $strSQL .= "	CONCAT(tbproduto.sDescricao, ' / QTDE: ',tbproduto.iquantidade) AS CONCAT, ";
                     $strSQL .= "	tbproduto.sDescricao ";
                     $strSQL .= " FROM tbproduto";
@@ -115,52 +132,48 @@
                     echo "<select key='btnFormEncerrar' name='idProduto' id='idProduto' class='form-control' style='display:block;'>";
                     echo "<option value=''></option>";
                     while ($rsDropDown = $retornoQuery->fetch_object())
-                      echo "<option value='" . $rsDropDown->id . "'>" . $rsDropDown->CONCAT . "</option>";
+                      echo "<option value='" . $rsDropDown->prod . "'>" . $rsDropDown->CONCAT . "</option>";
                     echo "</select>";
                     ?>
                   </div>
 
-                  <div class="form-group col-sm-1" style="padding-top: 35px; padding-left: 20px;">
+                  <div class="col-md-1 mb-3">
+                    <label for="quantidade">Qtde:</label>
+                    <input type="text" class="form-control" id="quantidade" value="" required>
+                    <div class="invalid-feedback">
+                      Valid first name is required.
+                    </div>
+                  </div>
+
+                  <script>
+                    var listProdutos = [];
+                  </script>
+                  <div class="form-group col-sm-1" onclick="addProduto()" style="padding-top: 35px; padding-left: 20px;">
                     <a><img src="plus.PNG" title="Adicionar Produto?" class="img" alt="Inserir"></a>
                   </div>
                 </div>
 
                 <div class="table-responsive">
-                  <table class="table table-striped table-sm">
+                  <table id="table-produto" class='table table-striped table-sm'>
                     <thead>
                       <tr>
-                        <th style="width: 5%">ID</th>
-                        <th style="width: 40%">DESCRICAO</th>
-                        <th style="width: 10%">QTD</th>
-                        <th style="width: 10%">PREÇO</th>
-                        <th style="width: 20%">TOTAL</th>
+                        <th style='width: 5%'>ID</th>
+                        <th style='width: 40%'>DESCRICAO</th>
+                        <th style='width: 10%'>QTD</th>
+                        <th style='width: 10%'>PREÇO</th>
+                        <th style='width: 20%'>TOTAL</th>
+                        <th style='width: 5%'>#</th>
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
-                        <td>001</td>
-                        <td>MESA</td>
-                        <td>2</td>
-                        <td>R$ 5.5</td>
-                        <td>R$ 11.0</td>
-                      </tr>
-                      <tr>
-                        <td>002</td>
-                        <td>CADEIRA</td>
-                        <td>3</td>
-                        <td>R$ 35.5</td>
-                        <td>R$ 106.5</td>
-                      </tr>
                     </tbody>
                   </table>
                 </div>
-
-
               </div>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-success"><i class="fa fa-share-square"></i>&nbsp; Salvar</i></button>
-              <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fa fa-times"></i>&nbsp; Sair</button>
+              <button type="button" onclick="add()" class="btn btn-success"><i class="fa fa-share-square"></i>&nbsp; Salvar</i></button>
+              <button type="button" onclick="sair()" class="btn btn-secondary" data-dismiss="modal"><i class="fa fa-times"></i>&nbsp; Sair</button>
             </div>
           </div>
         </div>
@@ -171,22 +184,20 @@
           <thead>
             <tr>
               <th style="width: 5%">ID</th>
+              <th style="width: 10%">STATUS</th>
               <th style="width: 20%">ALUNO</th>
-              <th style="width: 20%">PRODUTO</th>
-              <th style="width: 5%">QTD</th>
-              <th style="width: 20%">DATA VENDA</th>
-              <th style="width: 10%">PREÇO</th>
+              <th style="width: 10%">DATA VENDA</th>
               <th style="width: 10%">TOTAL</th>
+              <th style="width: 5%">#</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody id="body-venda">
             <?php
             $strSQL = "";
             $strSQL .= " SELECT ";
             $strSQL .= " 	id, ";
-            $strSQL .= " 	iquantidade, ";
-            $strSQL .= " 	fSubtotal, ";
-            $strSQL .= " 	fPreco, ";
+            $strSQL .= " 	istatus, ";
+            $strSQL .= " 	sCondicao , ";
             $strSQL .= " 	fTotal, ";
             $strSQL .= " 	sObservacao, ";
             $strSQL .= " 	dataVenda ";
@@ -194,14 +205,13 @@
             $strSQL .= " WHERE IFNULL(fDeletado, 0) = 0 ";
             if ($retornoQuery = $MySQLi->query($strSQL)) {
               while ($rsListagem = $retornoQuery->fetch_object()) {
-                echo "<tr>";
+                echo "<tr ondblclick='edit(" . $rsListagem->id . ")'>";
                 printf('<td>%s</td>', $rsListagem->id);
+                printf('<td>%s</td>', $rsListagem->istatus);
                 printf('<td>%s</td>', "joao");
-                printf('<td>%s</td>', "MESA");
-                printf('<td>%s</td>', $rsListagem->iquantidade);
                 printf('<td>%s</td>', $rsListagem->dataVenda);
-                printf('<td>R$ %s</td>', $rsListagem->fPreco);
                 printf('<td>R$ %s</td>', $rsListagem->fTotal);
+                printf("<td><i onclick='remove($rsListagem->id)' title='remover aluno?' class='far fa-times-circle'></i></td>");
                 echo "</tr>";
               }
             } else {
@@ -219,34 +229,144 @@
     ================================================== -->
   <!-- Placed at the end of the document so the pages load faster -->
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script>
-    window.jQuery || document.write('<script src="assets/js/vendor/jquery-slim.min.js"><\/script>')
-  </script>
   <script src="assets/js/vendor/popper.min.js"></script>
   <script src="dist/js/bootstrap.min.js"></script>
-
-  <!-- <script src="js/dist/jquery.js" type="text/javascript"></script>
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script> -->
-
   <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+
+
   <script>
     feather.replace()
   </script>
 
+  <script type="text/javascript" src="http://code.jquery.com/jquery-3.0.0.min.js"></script>
+  <script type="text/javascript" src="http://code.jquery.com/qunit/qunit-1.11.0.js"></script>
+
   <script>
-    add = function() {
-      alert("chamou!");
-      <?php
-      //include("formVenda.php");
-      ?>
-      /* $(".modal-title").html("Cadastro \\ Vendas");
-      $.post("formVenda.php", {idRegistro: 0, Action: 'Add'}, function(retorno){
-        $("#popup_modal").modal({backdrop: "static"});
-        $(".modal-completa").html(retorno);
-      }); */
-    };
+    function sair() {
+      window.location.reload();
+    }
+
+    function edit(idVenda) {
+      jQuery.ajax({
+        type: 'POST',
+        url: 'actVenda.php',
+        data: '&Action=editVenda' +
+          '&idVenda=' + idVenda,
+        success: function(html) {
+          $('#carregando').hide();
+          var mySplitResult = html.split("*");
+          document.getElementById('id').value = mySplitResult[0];
+          document.getElementById('status').value = mySplitResult[1];
+          document.getElementById('condicao').value = mySplitResult[2];
+          document.getElementById('observacao').value = mySplitResult[3];
+          jQuery.noConflict();
+          $("#modalVenda").modal({
+            show: true
+          });
+        }
+      });
+    }
+
+    function remove(idVenda) {
+      Swal.fire({
+        title: 'Deseja realmente remover Venda?',
+        text: "Dados será atualizados na base de dados!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim, remover!'
+      }).then((result) => {
+        if (result.value) {
+          jQuery.ajax({
+            type: 'POST',
+            url: 'actVenda.php',
+            data: '&Action=removeVenda' +
+              '&idVenda=' + idVenda,
+            beforeSend: function(html) {
+              $('#carregando').show();
+            },
+            success: function(html) {
+              $('#carregando').hide();
+              $('#body-venda').html(html);
+              Swal.fire(
+                'Deletado!',
+                'Removido com sucesso.',
+                'success'
+              )
+              setTimeout(function() {
+                window.location.reload(1);
+              }, 2000);
+            }
+          });
+        }
+      })
+    }
+
+    function add() {
+      var action = "";
+      if (document.getElementById("id").value == "")
+        action = "saveVenda";
+      else
+        action = "updateVenda";
+      jQuery.ajax({
+        type: 'POST',
+        url: 'actVenda.php',
+        data: '&Action=' + action +
+          '&idVenda=' + document.getElementById("id").value +
+          '&status=' + document.getElementById("status").value +
+          '&condicao=' + document.getElementById("condicao").value +
+          '&observacao=' + document.getElementById("observacao").value,
+        beforeSend: function(html) {
+          $('#carregando').show();
+        },
+        success: function(html) {
+          $('#carregando').hide();
+          $('#body-venda').html(html);
+          Swal.fire({
+            type: 'success',
+            title: 'Venda salva com sucesso!',
+            showConfirmButton: false,
+            timer: 2000
+          })
+          setTimeout(function() {
+            window.location.reload(1);
+          }, 2000);
+        }
+      });
+    }
+
+    function addProduto() {
+
+      var formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+      });
+
+      var result = document.getElementById("idProduto").value.split("*");
+      var total = parseInt(document.getElementById("quantidade").value) * parseFloat(result[2].replace('.', '').replace(',', '.'));
+      var formatado = formatter.format(total); 
+
+      var newRow = $("<tr>");
+      var cols = "";
+      cols += '<td>' + result[0] + '</td>';
+      cols += '<td>' + result[1] + '</td>';
+      cols += '<td>' + document.getElementById("quantidade").value + '</td>';
+      cols += '<td>' + result[2] + '</td>';
+      cols += '<td>' + formatado + '</td>';
+      cols += '<td>';
+      cols += '<i onclick="removeProd(this.parentNode.parentNode.rowIndex)" title="remover produto?" class="far fa-times-circle"></i>';
+      cols += '</td>';
+      newRow.append(cols);
+      $("#table-produto").append(newRow);
+      return false;
+
+    }
+
+    removeProd = function(i) {
+      document.getElementById('table-produto').deleteRow(i)
+    }
   </script>
 </body>
 
